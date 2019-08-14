e038a0b3b1bd28a7a119694cf7aefbe418271683
aaronlago <46574305+aaronlago@users.noreply.github.com>
2019-08-14 17:48:49 -0300

    Youtube React Estudo

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React from 'react'
+
+import { Grid } from '@material-ui/core'
+
+import { SearchBar, VideoDetail, VideoList } from './components'
+
+
+import youtube from './api/youtube'
+
+class App extends React.Component {
+  state = {
+    video: [],
+    selectedVideo: null,
+  }
+
+  componentDidMount() {
+    this.handleSubmit('curso js')
+  }
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video })
+  }
+  
+  handleSubmit = async (searchTerm) => {
+    const response = await youtube.get('search', { 
+      params:{
+        part: 'snippet',
+        maxResults: 5,
+        key: 'AIzaSyCNlT6cS5rjs128YiT5NpLnUqwOtLqtrjs',
+        q: searchTerm,
+      }
+    })
+
+    this.setState({ videos: response.data.items, selectedVideo: response.data.items[0] })
+  }
+  render () {
+    const { selectedVideo, videos } = this.state
+    return(
+      <Grid justify="center" container spacing={10}>
+        <Grid itemxs={12}>
+          <Grid container spacing={10}>
+            <Grid item xs={12}>
+            <SearchBar onFormSubmit={this.handleSubmit} />
+            </Grid>
+          </Grid>
+          <Grid item xs={8}>
+              <VideoDetail video={selectedVideo} />
+          </Grid>
+          <Grid item xs={4}>
+            <VideoList videos={videos} onVideoSelect={this.onVideoSelect}/>
+          </Grid>
+        </Grid>
+      </Grid>
+    )
+  }
+}
+
+export default App
